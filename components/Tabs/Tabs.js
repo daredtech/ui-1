070b3fbb3869b0d72e1b2af0406cc5d404ec0986
Red <vhanchar@ucsc.edu>
2019-06-26 09:37:10 -0700

    testing logic for the tabs

--- components/Tabs/Tabs.js
+++ components/Tabs/Tabs.js
@@ -1,4 +1,5 @@
-window.addEventListener('load', (event) => {
-  console.log('page is fully loaded');
-  // document.getElementById
-});
+
+let dummyContent = document.querySelectorAll('.content-tabs');
+let dummy = document.querySelectorAll('.tab');
+//dummy[0].addEventListener('click', () => console.log("CLICKED"));
+dummy[0].addEventListener('click', () => dummyContent[0].style.display = 'flex');
